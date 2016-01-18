9017bd226451d7316ebc0b42c406415b446f6536
Kris Jordan <krisjordan@gmail.com>
2016-01-18 14:53:06 -0500

    Initial commit of PS00 Hello World Meme.

---
+++ src/comp110/MemeGenerator.java
@@ -0,0 +1,18 @@
+/*
+​ * Author: YOUR NAME
+​ * ONYEN: YOUR ONYEN
+​ * Collaborators: ONYENS
+​ * UNC Honor Pledge: I certify that no unauthorized assistance has been 
+​ * received or given in the completion of this work.
+​ */
+package comp110;
+
+class MemeGenerator {
+  
+  void generate(MemePhoto meme) {
+    
+    // Your code will go here...
+    
+  }
+
+}
---
+++ src/comp110/PS00Runner.java
@@ -0,0 +1,12 @@
+/* SUPPORT CODE - DO NOT MODIFY */
+
+// We'll be working in the comp110 package this semester.
+package comp110;
+
+public class PS00Runner extends PS00Base {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
